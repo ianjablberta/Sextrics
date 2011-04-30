f94089c59dba3ce4e0ea9bf69d2ca38c692e6552
mendicant <mendicant@beigesunshine.com>
2011-04-30 10:18:28 -0600

    Get the project set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sextrics::Application.config.secret_token = '91c65ad6c9139550f4ab5eed3505f2900841d97fca9e7df37062011691c0af03817af9b329415707c87cb526bd52722c8cb572cad8ff6a3984161f00a36aa78f'
