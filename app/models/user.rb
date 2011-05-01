8aff22d1e8e7cd138f2605b84daf5a9ba3278de6
mendicant <mendicant@beigesunshine.com>
2011-04-30 18:03:00 -0600

    Add activities to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 
+  has_many :activities
+
   validates_uniqueness_of :username
 
+  def recent_activities
+    return self.activities.order('activities.date_of_activity DESC')
+  end
+
+  def ten_most_recent_activities
+    return recent_activities.limit(10)
+  end
 end
